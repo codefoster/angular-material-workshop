2c71ca508a2fabeaaecf696062f370dd06cc45da
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-09-17 15:35:18 -0500

    initial raw snapshot

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import {AppComponent} from './app.component';
+import {DialogComponent} from './dialog/dialog.component';
+
+import 'hammerjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DialogComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  entryComponents: [DialogComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
