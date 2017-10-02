272fad2bc2f481f5c1a7a4eff6686b67d6443ff2
Noam Okman <noamokman@gmail.com>
2017-10-02 19:03:54 +0300

    Step 3

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,14 +2,18 @@ import {NgModule} from '@angular/core';
 import {
   MdButtonModule,
   MdSidenavModule,
-  MdToolbarModule
+  MdToolbarModule,
+  MdTabsModule,
+  MdListModule
 } from '@angular/material';
 
 @NgModule({
   exports: [
     MdButtonModule,
     MdSidenavModule,
-    MdToolbarModule
+    MdToolbarModule,
+    MdTabsModule,
+    MdListModule
   ]
 })
 export class MaterialModule {}
